2e2f2f0484f9c877303173d74dfd4609bb35362f
Chandan Kumar <chandan181singh@gmail.com>
2023-05-10 06:45:56 +0530

    push

--- ex3c.sh
+++ ex3c.sh
@@ -1,9 +1,9 @@
-n=0
-file="/Users/ShresthS/Desktop/CSE/ASSGN/SEM4/shll/25-01-23/ex3in.txt"
-while IFS= read -r line
-do
-    n=$line 
-done < "$file"
+n=$(cat ex3in.txt)
+# file="ex3in.txt"
+# while IFS= read -r line
+# do
+#     n=$line 
+# done < "$file"
 
 for (( i=1; i<=10; i++ ))
 do
--- ex4a.sh
+++ ex4a.sh
@@ -1,12 +1,25 @@
-iter=1
+# iter=1
 
+# read -p "Enter new dir : " dirname
+# mkdir $dirname
+# read -p "Enter number of files : " nfile
+
+# while [  $iter -le $nfile ]
+# do
+# 	read -p "Enter file name : " filename
+# 	cp $filename $dirname
+# 	iter=$(( iter + 1 ))
+# done
+
+
+i=1
 read -p "Enter new dir : " dirname
 mkdir $dirname
-read -p "Enter number of files : " nfile
+read -p "Enter the no. of files :" n
 
-while [  $iter -le $nfile ]
-do
-	read -p "Enter file name : " filename
-	cp $filename $dirname
-	iter=$(( iter + 1 ))
-done
+while [ $i -le $n ]
+do 
+  read -p "enter name of file" filename
+  cp $filename $dirname
+  ((++i))
+done  
